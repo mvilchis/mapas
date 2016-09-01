c13ad84e7c6ef71d20586c0f88b9987c577e6e69
mvilchis <mvilchis@ciencias.unam.mx>
2016-09-01 18:10:30 -0500

    Version 1.0

--- lamp_server/postgis_run.sh
+++ lamp_server/postgis_run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 service postgresql start
+export PGUSER=postgres
+unzip db.out.zip
+
+sudo -u postgres psql -f db.out postgres
--- lamp_server/tomcat_run.sh
+++ lamp_server/tomcat_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-if [ ! -f /.tomcat_admin_created ]; then
-/create_tomcat_admin_user.sh
-fi
-exec ${MY_HOME}/bin/catalina.sh run
+
+service tomcat7 stop
+service tomcat7 start
